fe955375eab8dd8b59327aab7633fbfb7845d87c
HarshikaAgr <ha542@njit.edu>
2025-04-13 15:06:34 -0400

    Update Product

--- website/changeplayboxproduct.inc.php
+++ website/changeplayboxproduct.inc.php
@@ -1,14 +1,10 @@
 <?php
-//Harshika Agrawal, IT202 & 004, Internet Applications, Phase 2, ha542@njit.edu
-?>
-
-<?php
 //include('playboxproduct.php');
 if (isset($_SESSION['login'])) {
    $productID = $_POST['productID'];
    $answer = $_POST['answer'];
    if ($answer == "Update product") {
-      $productID = $_POST['productID'];
+//    $productID = $_POST['productID'];
       $product = Product::findProduct($productID);
       $product->productID = $_POST['productID'];
       $product->productName = $_POST['productName'];
@@ -19,7 +15,29 @@ if (isset($_SESSION['login'])) {
       $product->wholeSalePrice = $_POST['wholeSalePrice'];
       $product->listPrice = $_POST['listPrice'];
       $DateCreated = date('Y-m-d H:i:s');
-      $result = $product->updateProduct();
+      if (!(Category::findCategory($product->categoryID))) {
+         echo "<h2>Sorry, you must enter a valid category ID number</h2>\n";
+      }
+      else if (!(is_numeric($product->wholeSalePrice))) {
+         echo "<h2>Sorry, you must enter a valid wholesale price</h2>\n";
+      }
+      else if (!(is_numeric($product->listPrice))) {
+         echo "<h2>Sorry, you must enter a valid list price</h2>\n";
+      }
+      else{
+         $result = $product->updateProduct();
+         echo "<h2>Product $productID updated</h2>\n";
+      } 
+   }
+   else {
+      echo "<h2>Update Canceled for product $productID</h2>\n";
+   }
+} else {
+echo "<h2>Please login first</h2>\n";
+}
+?>
+
+<!--  $result = $product->updateProduct();
       if ($result) {
          echo "<h2>product $productID updated</h2>\n";
       } else {
@@ -31,4 +49,4 @@ if (isset($_SESSION['login'])) {
 } else {
    echo "<h2>Please log in first</h2>\n";
 }
-?>
+?> --> 
