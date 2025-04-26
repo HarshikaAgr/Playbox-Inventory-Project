3ae42b9d2196a95e9ff19fc97c949c8650a6e99e
HarshikaAgr <ha542@njit.edu>
2025-04-25 23:06:44 -0400

    logout

--- website/logout.inc.php
+++ website/logout.inc.php
@@ -7,6 +7,7 @@ if (isset($_SESSION['login'])) {
    unset($_SESSION['firstName']);
    unset($_SESSION['lastName']);
    unset($_SESSION['pronouns']);
+   session_destroy();
 }
 if (headers_sent()) {
    echo "Click <a href=\"index.php?content=logout\"><strong>here</strong></a> to logout.";
